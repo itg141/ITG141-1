4325f5800030f44713f35f97e71f00dd0e390171
chaitanya5019 <gck0817@gmail.com>
2021-08-27 13:00:15 +0530

    Firstcommit

--- Bhargav/src/basicquestion/Spellnumberstowords.java
+++ Bhargav/src/basicquestion/Spellnumberstowords.java
@@ -1,10 +1,12 @@
 package basicquestion;
-
+// Spell the number in form of words related to digits.
 public class Spellnumberstowords {
+	void Return(int num) {
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
