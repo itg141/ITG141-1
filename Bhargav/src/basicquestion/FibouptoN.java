2250bca8dcbd021f31ca4c3caeac9a82a3959e39
chaitanya5019 <gck0817@gmail.com>
2021-08-27 16:10:46 +0530

    First Commit

--- Bhargav/src/basicquestion/FibouptoN.java
+++ Bhargav/src/basicquestion/FibouptoN.java
@@ -3,25 +3,14 @@ package basicquestion;
 public class FibouptoN {
 	void Return(int a,int b,int n) {
 		//int sum=0;
-		if(n%2==0) {
+		
 			for(int i=1;i<=n/2;i++) {
 				a=a+b;
 				System.out.println(a);
 				//sum=sum+i;
 				b=a+b;
 				System.out.println(b);
-			}
-			
-		}
-		else {
-		for(int i=1;i<=(n/2);i++) {
-			a=a+b;
-			System.out.println(a);
-			b=a+b;
-			System.out.println(b);
-		}
-		}
-		
+				}	
 	}
 	public static void main(String[] args) {
 		FibouptoN f= new FibouptoN();
@@ -30,3 +19,4 @@ public class FibouptoN {
 	}
 
 }
+///// vague answer is produced, logical error: recheck this later(interesting)
---
+++ Bhargav/src/basicquestion/OccurDigit.java
@@ -0,0 +1,10 @@
+package basicquestion;
+// Program to print the no of occurrences of a digit in a number. 
+public class OccurDigit {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
