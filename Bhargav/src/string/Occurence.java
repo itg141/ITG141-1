f3579834372d6d68c1647a1c67facabfaff931b7
chaitanya5019 <gck0817@gmail.com>
2021-08-31 00:11:30 +0530

    String Commit

--- Bhargav/src/string/Occurence.java
+++ Bhargav/src/string/Occurence.java
@@ -1,10 +1,39 @@
 package string;
-
+import java.util.Scanner;
 public class Occurence {
-
+///logic reffered
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str;
+		int flag=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string");
+		str=sc.nextLine();
+		sc.close();
+		int ln=0;
+		char ch[]=new char[str.length()];
+		for(int i=0; i<str.length();i++) {
+		flag=0;
+		for(int j=0; j<str.length();j++) {
+		if(str.charAt(i)==str.charAt(j)) {
+		flag=flag+1;
+		}
 
+		}
+		int m=1;
+		for(int k=0;k<ch.length;k++) {
+		if(ch[k]==str.charAt(i)) {
+		m=0;
+		}
+		}
+		if(m==1) {
+		ch[ln]=str.charAt(i);
+		ln++;
+		}
+
+		if(m==1) {
+		System.out.println(str.charAt(i)+" : "+flag);
+		}
+		}
 	}
 
 }
