dd17eebad8ac79dbe900bc07bd0ddceedea59dc9
chaitanya5019 <gck0817@gmail.com>
2021-09-03 20:52:55 +0530

    Commit

---
+++ Bhargav/src/Methodoverriding/MarkerInterface.java
@@ -0,0 +1,8 @@
+package Methodoverriding;
+// an interface with zero methods is called a marker interface
+public interface MarkerInterface {
+
+}// this is a markerinterface
+class Zero implements Markerinterface{
+	
+}
